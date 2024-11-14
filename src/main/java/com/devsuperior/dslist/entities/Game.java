ad7012d0ff1a9b8dc409c4f012298f1eddfc4c3e
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2024-11-14 00:22:29 -0300

    Add import SQL externo para popular dados da tabela_game

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,22 +20,34 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    /* Esta anotação acima do shortDescription e longDescription, com esta config.
+    estar relaconada com a quantidade de carcteres da descrição do atributo, pois
+    sem esta config. ela aceita por padrão no banco de dados, apenas 255 caracteres,
+    sendo assim, pouco para uma descrição longa e também curta do atributo. Usando o
+     "TEXT" essa capacidade aumenta, assim satisfazendo a necessidade do projeto.
+     */
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
                 String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -73,12 +85,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
     public void setPlatform(String platform) {
-        this.platform = platform;
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -121,4 +141,6 @@ public class Game {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
